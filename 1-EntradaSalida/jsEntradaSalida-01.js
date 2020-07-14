78bc3ad28a833e63c349d5283153364f56661e8e
Julián González <julian.gonzalezca@gmail.com>
2020-07-13 22:46:14 -0300

    Tercer commit

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,9 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Bienvenidos DA");
+	alert("Esto lo escribí primero");
+	alert("Esto lo escribí segundo");
+	alert("Esto lo escribí tercero");
 }
+
 
