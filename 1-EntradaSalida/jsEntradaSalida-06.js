17b3ea319650150e73540036879af80d50a4b158
Julián González <julian.gonzalezca@gmail.com>
2020-07-14 22:36:38 -0300

    Cuarto commit

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,26 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	let num1;
+	let num2;
+	let resultado;
+
+	num1 = parseInt (document.getElementById("txtIdNumeroUno").value);
+
+	num2 = parseInt (document.getElementById("txtIdNumeroDos").value);
+
+    //se puede convertir el valor en numero de ambas maneras, en la cursada se hara de la forma anterior
+	//num1 = document.getElementById("txtIdNumeroUno").value; 
+
+	//num2 = document.getElementById("txtIdNumeroDos").value; 
+
+	//num1 = parseInt (num1);
+
+	//num2 = parseInt (num2);
+
+	resultado = num1 + num2;
+
+
+	alert("La suma es " + resultado);
 }
 
